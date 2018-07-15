00902aa25720ccd8fc0801ff82040195e48ac8b8
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-15 17:43:07 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,20 @@
+class Dog
+  def initialize
+    @name = fido
+    @breed = mutt
+  end
+end
+
+  def breed
+    @breed
+  end
+
+  def breed=(breed)
+    @breed = breed
+  end
+
+
+
+fido = Dog.new("fido")
+
+fido.breed
