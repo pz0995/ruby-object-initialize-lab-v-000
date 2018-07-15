fd1e60b10d8560cbb37a83495fd914fdc91a5438
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-15 19:16:47 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,13 @@
 class Dog
-  def initialize(breed)
+  def initialize(name, breed)
     @name = name
     @breed = breed
   end
 end
 
+def name=(name)
+    @name = name
+end
   def breed=(breed)
     @breed = breed
   end
